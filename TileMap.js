90a0a126b80f38b33f9d68920abea276a6721e42
NamDo03 <116558889+NamDo03@users.noreply.github.com>
2024-03-08 21:07:50 +0700

    class Wall (#1)
    * class Wall
    
    * add map

--- TileMap.js
+++ TileMap.js
@@ -1,45 +1,69 @@
 export default class TileMap {
-    constructor(config, map) {
-        this.tileSize = config.tileSize;
+    constructor(canvas, mapConfig, walls) {
+        this.canvas = canvas
+        this.mapConfig = mapConfig
+        this.map
+        this.tileSize
+        this.imgSize
+        this.walls = walls
+    }
+    updateMap(config, map) {
+        let { tileSize, imgSize } = config
+        this.tileSize = tileSize
+        this.imgSize = imgSize
         this.map = map
-        this.wall = this.#image(`${config.wall}`);
     }
 
-    #image(fileName) {
-        const img = new Image();
-        img.src = `images/${fileName}`;
-        return img;
+    setMap(level, mapNumber) {
+        let selectedConfig = this.mapConfig[level];
+        let selectedMap = selectedConfig.maps[mapNumber];
+        if (!selectedConfig || !selectedMap) {
+            console.log("Not found map: " + level + "." + mapNumber)
+            return
+        }
+        this.updateMap(selectedConfig, selectedMap)
     }
 
-    draw(canvas, ctx) {
-        this.#setCanvasSize(canvas);
-        this.#drawMap(ctx);
+    draw(level, mapNumber) {
+        try {
+            this.setMap(level, mapNumber)
+            this.#setCanvasSize();
+            this.#drawBackground();
+            this.#drawMap();
+        } catch (e) {
+            console.log("Error when draw map:", e)
+        }
     }
 
-    #drawMap(ctx) {
+    #drawMap() {
         for (let row = 0; row < this.map.length; row++) {
             for (let column = 0; column < this.map[row].length; column++) {
                 const tile = this.map[row][column];
-                let image = null;
                 switch (tile) {
                     case 1:
-                        image = this.wall
+                        this.walls.drawWallLeft(this.imgSize, column, row, this.tileSize);
+                        break;
+                    case 2:
+                        this.walls.drawWallRight(this.imgSize, column, row, this.tileSize);
+                        break;
+                    case 3:
+                        this.walls.drawWallTop(this.imgSize, column, row, this.tileSize);
+                        break;
+                    default:
                         break;
                 }
-                if (image != null)
-                    ctx.drawImage(
-                        image,
-                        column * this.tileSize,
-                        row * this.tileSize,
-                        this.tileSize,
-                        this.tileSize
-                    );
             }
         }
     }
 
-    #setCanvasSize(canvas) {
-        canvas.height = this.map.length * this.tileSize;
-        canvas.width = this.map[0].length * this.tileSize;
+    #drawBackground() {
+        let gameElement = document.getElementById('game');
+        gameElement.style.backgroundImage = `url('images/floor${this.imgSize}.jpg')`;
+
+    }
+
+    #setCanvasSize() {
+        this.canvas.height = this.map.length * this.tileSize;
+        this.canvas.width = this.map[0].length * this.tileSize;
     }
 }

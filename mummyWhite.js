363eeb1c1a9320ae12a3f15aa4305a625a80d955
SonBuik3 <112226878+SonBuik3@users.noreply.github.com>
2024-04-03 21:08:03 +0700

    thuat toan Mummymove (#10)
    * mummymove
    
    * thuat toan

--- mummyWhite.js
+++ mummyWhite.js
@@ -17,10 +17,54 @@ export default class MummyWhite {
         this.frameY = 2;
         this.isAnimating = false;
         this.isMoving = false;
-        // document.addEventListener("keydown", this.#keydown.bind(this));
-
     }
+    move(playerPosition) {
+        if (this.isAnimating || this.isMoving) return;
+        this.previousPosition.x = this.position.x;
+        this.previousPosition.y = this.position.y;
+        let possibleMoves = []; // Lưu trữ các bước di chuyển có thể
+        const { x: playerX, y: playerY } = playerPosition;
+        const { x, y } = this.position;
+        
+        // Tính khoảng cách Manhattan giữa xác ướp và người chơi
+        const manhattanDistance = Math.abs(playerX - x) + Math.abs(playerY - y);
 
+        // Kiểm tra nếu khoảng cách đã đạt tối đa, thì không cần di chuyển nữa
+        if (manhattanDistance === 0) return;
+
+        // Thử tất cả các hướng di chuyển và lưu vào mảng possibleMoves
+        if (y > playerY && this.movable.left) possibleMoves.push({ x, y: y - 1 });
+        if (y < playerY && this.movable.right) possibleMoves.push({ x, y: y + 1 });
+        if (x > playerX && this.movable.up) possibleMoves.push({ x: x - 1, y });
+        if (x < playerX && this.movable.down) possibleMoves.push({ x: x + 1, y });
+
+        // Lựa chọn hướng di chuyển
+        let chosenMove = possibleMoves[0]; // Mặc định là hướng đầu tiên
+        let minDistance = Math.abs(playerX - chosenMove.x) + Math.abs(playerY - chosenMove.y);
+        // Tìm hướng di chuyển tối ưu để giảm khoảng cách Manhattan
+        for (let i = 1; i < possibleMoves.length; i++) {
+            const move = possibleMoves[i];
+            const distance = Math.abs(playerX - move.x) + Math.abs(playerY - move.y);
+            if (distance < minDistance) {
+                chosenMove = move;
+                minDistance = distance;
+            }
+        }     
+        // Cập nhật hình ảnh sau cùng khi mummy di chuyển
+        if (chosenMove.y > this.previousPosition.y) {
+            this.frameY = 1; 
+        } else if (chosenMove.y < this.previousPosition.y) {
+            this.frameY = 3; 
+        } else if (chosenMove.x < this.previousPosition.x) {
+            this.frameY = 0; 
+        } else if (chosenMove.x > this.previousPosition.x) {
+            this.frameY = 2; 
+        }
+            this.isAnimating = true;            
+            this.isMoving = true;
+            this.targetPosition = {x:chosenMove.x,y:chosenMove.y};
+            this.animateInterpolation();
+    }  
     draw(size, tileSize) {
         //xoa hinh anh o vi tri cu
         if (this.previousPosition.x !== null && this.previousPosition.y !== null) {
@@ -62,7 +106,6 @@ export default class MummyWhite {
                 this.frameX = 0;
                 this.isAnimating = false;
             }
-            console.log(this.frameX)
             if (this.isAnimating) {
                 setTimeout(() => {
                     requestAnimationFrame(animateFrame);
@@ -73,51 +116,6 @@ export default class MummyWhite {
             animateFrame();
         }
     }
-
-
-    // #keydown = (event) => {
-    //     if (this.isAnimating || this.isMoving) return;
-    //     this.previousPosition.x = this.position.x;
-    //     this.previousPosition.y = this.position.y;
-
-    //     let targetX = this.position.x;
-    //     let targetY = this.position.y;
-    //     switch (event.keyCode) {
-    //         case 38: // up arrow
-    //             if (this.movable.up) {
-    //                 targetX--;
-    //                 this.frameY = 0;
-    //                 this.isMoving = true;
-    //             }
-    //             break;
-    //         case 40: // down arrow
-    //             if (this.movable.down) {
-    //                 targetX++;
-    //                 this.frameY = 2;
-    //                 this.isMoving = true;
-    //             }
-    //             break;
-    //         case 37: // left arrow
-    //             if (this.movable.left) {
-    //                 targetY--;
-    //                 this.frameY = 3;
-    //                 this.isMoving = true;
-    //             }
-    //             break;
-    //         case 39: // right arrow
-    //             if (this.movable.right) {
-    //                 targetY++;
-    //                 this.frameY = 1;
-    //                 this.isMoving = true;
-    //             }
-    //             break;
-    //         default:
-    //             return;
-    //     }
-    //     this.targetPosition = { x: targetX, y: targetY };
-    //     this.isAnimating = true;
-    //     this.animateInterpolation();
-    // };
 
     animateInterpolation() {
         const duration = 450;
